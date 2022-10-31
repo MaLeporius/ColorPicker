162776ec103640e0587b1da2963e46108ac675b8
Manuel Lepore <manuel1903@live.it>
2022-10-31 17:52:30 +0100

    Finale 1.0

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ document.getElementById("btn4").addEventListener("click", blu);
 document.getElementById("btn5").addEventListener("click", viola);
 document.getElementById("btn6").addEventListener("click", arancione);
 
+function refresh(){
+    window.location.reload("Refresh")
+  }
+
 function rosso() {
    document.body.style.backgroundColor = "red";
    document.body.style.color = "white";
